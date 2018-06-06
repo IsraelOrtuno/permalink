3bb22047b1253f315edfc5372820ef7fbda44704
Israel Ortuño <ai.ortuno@gmail.com>
2018-06-06 19:38:46 +0200

    Added the permalink macro

--- src/PermalinkServiceProvider.php
+++ src/PermalinkServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace Devio\Permalink;
 
+use Illuminate\Routing\Route;
 use Devio\Permalink\Routing\Router;
 use Illuminate\Support\ServiceProvider;
 use Devio\Permalink\Routing\ActionResolver;
@@ -15,7 +16,25 @@ class PermalinkServiceProvider extends ServiceProvider
     {
         $this->loadMigrationsFrom(__DIR__ . '/../migrations');
 
-//        (new Router($this->app['router'], new ActionResolver))->load();
+        $this->definePermalinkMacro();
+
+        (new Router($this->app['router'], new ActionResolver))->load();
+    }
+
+    protected function definePermalinkMacro()
+    {
+        // Adding a permalink macro to the Route object will let us store a
+        // permalink model instance right directly into the route, we can
+        // then access to this instance from the current Route object.
+        Route::macro('permalink', function ($permalink = null) {
+            if (is_null($permalink)) {
+                return $this->permalink;
+            }
+
+            $permalink->setRelations([]);
+
+            $this->permalink = $permalink;
+        });
     }
 
     /**
