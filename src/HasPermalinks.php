ce5be2478a9d9a06aec85ecf8939a414a2d0850e
Israel Ortuño <ai.ortuno@gmail.com>
2018-06-08 17:48:08 +0200

    Added observer

--- src/HasPermalinks.php
+++ src/HasPermalinks.php
@@ -4,19 +4,37 @@ namespace Devio\Permalink;
 
 trait HasPermalinks
 {
+    protected $permalinkAttributes = null;
+
     /**
      * Booting the trait.
      */
     public static function bootHasPermalinks()
     {
-        // When an entity has been fully created and stored, we will check if
-        // the automatic creation of permalinks is enabled. If so, we will
-        // just create the permalink accordingly to the new stored model.
-        static::created(function ($model) {
-            if ($model->createPermalink ?? true) {
-                $model->permalink()->create();
-            }
-        });
+        static::observe(PermalinkObserver::class);
+    }
+
+    /**
+     * Set the permalink attributes.
+     *
+     * @param $value
+     */
+    public function setPermalinkAttribute($value)
+    {
+        // This method is supposed to be used to store the permalink data from
+        // the request. This data can later be retrieved by the saving event
+        // and stored into the permalinks table. This is a kind of bridge.
+        $this->permalinkAttributes = $value;
+    }
+
+    /**
+     * Get the permalink attributes if any.
+     *
+     * @return null
+     */
+    public function getPermalinkAttributes()
+    {
+        return $this->permalinkAttributes;
     }
 
     /**
---
+++ src/PermalinkObserver.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Devio\Permalink;
+
+use Devio\Permalink\Contracts\Permalinkable;
+use Illuminate\Http\Request;
+use Illuminate\Database\Eloquent\Model;
+
+class PermalinkObserver
+{
+    public function created(Model $model)
+    {
+        if (! $this->managePermalinks($model)) {
+            return;
+        }
+
+        $model->permalink()->create($this->gatherAttributes($model));
+    }
+
+    /**
+     * Saved model event handler.
+     *
+     * @param Model $model
+     */
+    public function updated(Model $model)
+    {
+        if (! $this->managePermalinks($model) || ! $model->permalink) {
+            return;
+        }
+
+        $model->permalink()->update($this->gatherAttributes($model));
+    }
+
+    /**
+     * Check if the model should auto manage the permalinks.
+     *
+     * @param Model $model
+     * @return bool|mixed
+     */
+    protected function managePermalinks(Model $model)
+    {
+        return $model->managePermalinks ?? true;
+    }
+
+    /**
+     * Get the attributes from the request or 'permalink' key.
+     *
+     * @param null $attributes
+     * @return array
+     */
+    protected function gatherAttributes($attributes = null)
+    {
+        if ($attributes instanceof Permalinkable) {
+            return $attributes->getPermalinkAttributes();
+        }
+
+        $attributes = $attributes ?: request();
+
+        return ($attributes instanceof Request ? $attributes->get('permalink') : $attributes) ?? [];
+    }
+}
