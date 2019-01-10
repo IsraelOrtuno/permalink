7ec15283ea8f89807b578e6910ed70b7b8b3cb7e
Israel Ortuño <ai.ortuno@gmail.com>
2019-01-10 19:49:59 +0100

    Added PermalinkSeo

--- src/PermalinkServiceProvider.php
+++ src/PermalinkServiceProvider.php
@@ -58,7 +58,7 @@ class PermalinkServiceProvider extends ServiceProvider
         $this->app->singleton('router', Router::class);
 
         $this->app->singleton(Contracts\RequestHandler::class, function () {
-            return new RequestHandler($this->app['request'], $this->app);
+            return new PermalinkSeo($this->app['request'], $this->app);
         });
 
         $this->app->singleton(PermalinkManager::class, function () {
